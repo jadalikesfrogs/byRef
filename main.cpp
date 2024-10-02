05fc09b68b950a1812bae5d2842a5587dbfd2bf2
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-02 08:48:10 -0600

    correctly swaps values

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 
 using namespace std;
+/*
+Functon exchange 
+param[in, out] int a the first value to be exchanged
+param[in, out] int b the second value to be exchanged
+returns none
+*/
+// " & " the address of 
+void swap(int& a , int& b);
+
+int main() {
+	int a = 10, b = 20;
+
+	swap(a, b);
+
+	cout << a << ", " << b << endl;
+}
+
+void swap(int& a, int& b)
+{
+	int temp = b;
+	b = a;
+	a = temp;
+}
